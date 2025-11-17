ce7097de5cb358204db28ee69ecb0f0e2353d6ab
Arunav Lamba <axl110027@gmail.com>
2025-11-17 14:09:18 -0500

    VGA
    Local parameters and PLL instantiated

--- vgaController.sv
+++ vgaController.sv
@@ -1,5 +1,19 @@
 `default_nettype none
 module vgaController(
-    input logic 
+    input logic enable, reset,
+    output logic hsync, vsync
 );
+    // VGA timing specifications
+    localparam horiz_pixel = 640;
+    localparam vert_pixel = 480;
+    localparam horiz_front_porch = 16;
+    localparam vert_front_porch = 10;
+    localparam horz_sync_pulse = 96;
+    localparam vert_sync_pulse = 2;
+    localparam horiz_back_porch = 48;
+    localparam vert_back_porch = 33;
+
+    // PLL Instantiation
+    pll_clkGen u0 ();
+
 endmodule
