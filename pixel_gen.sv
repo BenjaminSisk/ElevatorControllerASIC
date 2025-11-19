4cb7dd7671806c473ac6b512e9c67f0d43db03a9
Arunav Lamba <axl110027@gmail.com>
2025-11-19 09:45:23 -0500

    Vga update
    VGA controller, remaining work is one the pixel generation

---
+++ pixel_gen.sv
@@ -0,0 +1,6 @@
+module pixel_gen (
+    input logic enable,
+    output logic [3:0]R, [3:0]G, [3:0]B
+);
+
+endmodule
--- vgaController.sv
+++ vgaController.sv
@@ -2,7 +2,6 @@
 module vgaController(
     input logic enable, reset,
     output logic hsync, vsync, drawn,
-    output logic [3:0]R, [3:0]G, [3:0]B,
     output logic [9:0]x_coord, y_coord
 );
     // VGA timing specifications
@@ -19,10 +18,10 @@ module vgaController(
     logic pixel_clk;
     pll_clkGen pll_u0 (.VGA_CLK(pixel_clk));
 
-    // Counter values and enable 
+    // Counter values and local enable for the pixel generator
     logic [9:0] horiz_count;
     logic [9:0] vert_count;
-    logic enable;
+    logic display_enable;
 
     // Output RBG and sync values
     always_ff @(posedge pixel_clk, posedge reset) begin
@@ -30,23 +29,51 @@ module vgaController(
             horiz_count <= 0;
             vert_count <= 0;
             enable <= 0;
+            // Hsync and Vsync are active low signals
             hsync <= 1'b1;
             vsync <= 1'b1;
-            R <= 4'b0000;
-            G <= 4'b0000;
-            B <= 4'b0000;
+        end
+        else begin
+            counterParametric #(.COUNT(800), .WIDTH(10)) horiz_counter (.clk(pixel_clk), .rst(reset), .counter(horiz_count))
+            begin
+                if (horiz_count > horiz_back_porch + horiz_front_porch + horiz_pixel + horz_sync_pulse - 1) begin
+                    counterParametric #(.COUNT(525), .WIDTH(10) vert_counter (.clk(pixel_clk), .rst(reset), .counter(vert_count)))
+                end
+            end
         end
     end
 
     // Horizontal logic
     always_comb begin
-        
+        if ((horiz_count >= horiz_pixel + horiz_front_porch) && (horiz_count < horiz_pixel + horiz_front_porch + horz_sync_pulse)) begin
+            hsync = 1'b0;
+        end
+        else begin
+            hsync = 1'b1;
+        end
     end
 
     // Vertical logic
     always_comb begin
-
+        if ((vert_count >= vert_pixel + vert_front_porch) && (vert_count < vert_pixel + vert_front_porch + vert_sync_pulse)) begin
+            vsync = 1'b0;
+        end
+        else begin
+            vsync = 1'b1;
+        end
     end
-    
+
+    // Display logic
+    always_comb begin
+        if (enable && horiz_count < horiz_pixel && vert_count < vert_pixel) begin
+            display_enable = 1'b1;
+        end
+        else begin
+            display_enable = 1'b0
+        end
+    end
+
+
+
 
 endmodule
