54d76612e61d76a18020f3f870529ed17df89da7
Arunav Lamba <axl110027@gmail.com>
2025-11-30 15:19:24 -0500

    top included the vgatest module and not the actual module

--- source/top.sv
+++ source/top.sv
@@ -3,7 +3,7 @@ module top (
     ICE_28, ICE_31, ICE_32, ICE_34, ICE_36, ICE_38
 );
 
-    vgatest vgacon(.reset(1'b0), .hsync(ICE_36), .vsync(ICE_38),
+    vgaController vgacon(.reset(1'b0), .hsync(ICE_36), .vsync(ICE_38),
         .R({ICE_44_G6, ICE_45, ICE_46, ICE_47}), .B({ICE_48, ICE_2, ICE_3, ICE_4}), 
         .G({ICE_28,ICE_31,ICE_32,ICE_34})
     );
