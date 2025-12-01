b2cb96d0a6d6d936cbdb224c547103f3b0e47cb5
Arunav Lamba <axl110027@gmail.com>
2025-12-01 11:49:12 -0500

    Trying to make this work

--- source/top.sv
+++ source/top.sv
@@ -1,16 +1,17 @@
 module top (
-    // Clk
-    input logic CLK,
     // VGA Pins
     output logic ICE_44_G6, ICE_45, ICE_46, ICE_47, ICE_48, ICE_2, ICE_3, ICE_4,
     ICE_28, ICE_31, ICE_32, ICE_34, ICE_36, ICE_38);
 
 
     logic [7:0] output_dest;
+    logic CLK;
+
+    pll_clkGen u2 (.VGA_CLK(CLK))
 
     counterParametric #(.COUNT(8'b1010_1010), .WIDTH(8)) output_stuff
     (
-        .counter(output_dest), .clk(CLK), .rst(1'b0), .en(1'b1)
+        .counter(output_dest), .clk(CLK), .rst(1'b0), .en(1'b1), .syncRst(1'b0)
     );
 
 
