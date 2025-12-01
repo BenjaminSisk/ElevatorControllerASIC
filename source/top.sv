fd9dbbc6441b38d39f9d830c2726495a57670bb5
Arunav Lamba <axl110027@gmail.com>
2025-12-01 12:04:14 -0500

    Made counter longer

--- source/top.sv
+++ source/top.sv
@@ -4,12 +4,12 @@ module top (
     ICE_28, ICE_31, ICE_32, ICE_34, ICE_36, ICE_38);
 
 
-    logic [7:0] output_dest;
+    logic [15:0] output_dest;
     logic CLK;
 
     pll_clkGen u2 (.VGA_CLK(CLK));
 
-    counterParametric #(.COUNT(8'b1010_1010), .WIDTH(8)) output_stuff
+    counterParametric #(.COUNT(16'b1), .WIDTH(16)) output_stuff
     (
         .counter(output_dest), .clk(CLK), .rst(1'b0), .en(1'b1), .syncRst(1'b0)
     );
